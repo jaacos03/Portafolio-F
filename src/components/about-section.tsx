18d715236489dd62ba848046e6a923aa42c403ea
AbdrielAcosta <133191785+AbdrielAcosta@users.noreply.github.com>
2024-05-30 19:43:17 -0600

    Cambios

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -30,17 +30,15 @@ const AboutSection = () => {
               Get to know me!
             </h1>
             <p>
-              Hi, my name is Hosna and I am a{" "}
+              Hi, my name is Jared and I am a{" "}
               <span className="font-bold">{"highly ambitious"}</span>,
               <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> software engineer
-              based in Los Angeles, CA.
+              <span className="font-bold">{" driven"}</span> student of software engineer
+              based in Mexico.
             </p>
             <br />
             <p>
-              I graduated from California State University, Northridge in 2019
-              with a BS in Computer Engineering and have been working in the
-              field ever since.
+              Im actually a studen in the Universidad Autonoma de Nuevo Leon.
             </p>
             <br />
             <p>
@@ -50,16 +48,7 @@ const AboutSection = () => {
               engaged and learning new things.
             </p>
             <br />
-            <p>
-              I believe that you should{" "}
-              <span className="font-bold text-teal-500">
-                never stop growing
-              </span>{" "}
-              and that&#39;s what I strive to do, I have a passion for
-              technology and a desire to always push the limits of what is
-              possible. I am excited to see where my career takes me and am
-              always open to new opportunities. 🙂
-            </p>
+            
           </div>
           <div className="text-center md:w-1/2 md:text-left">
             <h1 className="text-2xl font-bold mb-6">My Skills</h1>

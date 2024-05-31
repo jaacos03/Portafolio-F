80a9c00aa075a1a0f347f87b8746b14a74c31531
AbdrielAcosta <133191785+AbdrielAcosta@users.noreply.github.com>
2024-05-31 10:24:49 -0600

    Cambios

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -19,15 +19,14 @@ const HeroSection = () => {
         </div>
         <div className="md:mt-2 md:w-3/5">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
-            Hi, I&#39;m Hosna!
+            Hi, I&#39;m Jared!
           </h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
               Software Engineer{" "}
             </span>
-            based in Los Angeles, CA. Working towards creating software that
-            makes life easier and more meaningful.
+            Who wants to create solutions and optimize all processes that represents difficulties of processes.
           </p>
           <Link
             to="projects"
